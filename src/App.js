084e6c18944f0077154bcac832bb047fe784f96d
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-12-04 12:45:44 +0100

    Wraping Routing with context and global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Routing from "./routing/Routing.js"
+import {MainBody} from "./styles/global.style";
+import {UserProvider} from "./context/UserContext";
 
 function App() {
-  return (
-      <Routing />
-  );
+    return (
+        <MainBody>
+            <UserProvider>
+                <Routing/>
+            </UserProvider>
+        </MainBody>
+    );
 }
 
 export default App;
