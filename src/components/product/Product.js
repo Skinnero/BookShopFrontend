ec38bdea8162cb7c018f22f544f0dedd04004c37
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-12-04 12:52:00 +0100

    Refactoring product component and adding ability to add to basket whether user is logged in or not

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,26 +1,41 @@
-import "./Product.css"
-import {Card} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import Placeholder from "../../mockdata/dummy_placeholder.png"
-import {useEffect, useState} from "react";
-import {getProduct} from "../../api/Product";
+import {ProductButton, ProductCard, ProductCardBody, ProductPrice, ProductsGrid, ProductTitle} from "./Product.styles";
+import {useJsCookie} from "../../hook/UseJsCookie";
+import {BASKET} from "../../constants/Cookies";
+import {useContext} from "react";
+import {UserContext} from "../../context/UserContext";
 
-const Product = ({apiData}) => {
+const Product = ({products}) => {
+    const {user, addToBasket} = useContext(UserContext)
+    const {setCookie: setBasket, getCookie: getBasket} = useJsCookie(BASKET)
+
+    const handleAddToCart = (product) => {
+        if (!user.id) {
+            if (!getBasket()) {
+                setBasket(JSON.stringify([product]))
+            } else {
+                const oldBasket = JSON.parse(getBasket())
+                oldBasket.push(product)
+                setBasket(JSON.stringify(oldBasket))
+            }
+        } else {
+            addToBasket(product)
+        }
+    }
 
     return (
-        <div className={"products"}>
-            {apiData.map(product => (
-                <Card className={"item"} key={product.id} id={product.id}>
-                    <Card.Img variant={"top"} src={Placeholder}/>
-                    <Card.Body>
-                        <Card.Title>{product.name}</Card.Title>
-                        <Card.Subtitle>{product.price} {product.currency}</Card.Subtitle>
-                        <Card.Text>{product.description}</Card.Text>
-                        <Button variant={"primary"}>Add to Cart</Button>
-                    </Card.Body>
-                </Card>
+        <ProductsGrid>
+            {products.map(product => (
+                <ProductCard key={product.id}>
+                    <img src={Placeholder} alt={Placeholder}/>
+                    <ProductCardBody>
+                        <ProductTitle>{product.name}</ProductTitle>
+                        <ProductPrice>{product.price} {product.currency}</ProductPrice>
+                        <ProductButton id={product.id} onClick={() => handleAddToCart(product)}>Add to Cart</ProductButton>
+                    </ProductCardBody>
+                </ProductCard>
             ))}
-        </div>
+        </ProductsGrid>
     )
 }
 
