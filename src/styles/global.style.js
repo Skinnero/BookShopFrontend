5fdaf0ba734ff41611dc71c6fcd8964b4ba9f5a5
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-12-19 14:00:40 +0100

    Adding scroll styles

--- src/styles/global.style.js
+++ src/styles/global.style.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+const shadow = `.3rem .3rem .6rem #c8d0e7,
+-.2rem -.2rem .5rem #FFFFFF`
+
+const innerShadow = `inset .2rem .2rem .5rem #c8d0e7,
+inset -.2rem -.2rem .5rem #FFFFFF`
+
 export const MainBody = styled.div`
   margin: 0;
   padding: 0;
@@ -8,4 +14,19 @@ export const MainBody = styled.div`
   background-color: #E4EBF5;
   font-family: 'Quicksand', sans-serif;
   font-size: 20px;
+  
+  ::-webkit-scrollbar {
+    width: 1rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: ${shadow};
+    background: white;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: ${innerShadow};
+    border-radius: 10px;
+  }
 `
